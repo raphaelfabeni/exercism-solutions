719f86ddd2fd28858fe21d21769362b3eee6a58a
Raphael Fabeni <rapha.fabeni@gmail.com>
2015-03-05 20:09:47 -0300

    Adds a start for new exercise

---
+++ javascript/beer-song/beer-song.js
@@ -0,0 +1,28 @@
+var Beer = {};
+
+Beer.verse = function(verse) {
+  var amount, rest, phrase, action;
+  switch(verse) {
+    case 0:
+      amount = 'No more bottles ';
+      action = 'Go to the store and buy some more, '
+      rest = '99'
+      break;
+    case 1:
+      amount = '1 bottle ';
+      action = 'Take it down and pass it around, '
+      rest = 'no more'
+      break;
+    default:
+      amount = verse + ' bottles ';
+      action = 'Take one down and pass it around, '
+      rest = verse - 1;
+      break;
+  }
+
+  phrase = amount + 'of beer on the wall, ' + amount.toLoweCase() + 'of beer.\n' + action + rest + ' bottles of beer on the wall.\n';
+
+  return phrase;
+};
+
+module.exports = Beer;
